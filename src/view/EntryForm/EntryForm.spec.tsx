f52efac215530e2534667564c1305dddd3534d02
Yariv Gilad <yariv.gilad@gmail.com>
2023-08-01 08:49:35 +0300

    test: added unit tests

--- src/view/EntryForm/EntryForm.spec.tsx
+++ src/view/EntryForm/EntryForm.spec.tsx
@@ -1,15 +1,25 @@
 import { test, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { EntryForm } from "./EntryForm.view"; 
+import { EntryForm } from "./EntryForm.view";
+import { TodosContext, createStore } from '../../state/store.ts';
 
+let store = createStore();
+
+beforeEach(() => {
+    store = createStore()
+});
 
 test('Button disabled - when input text is empty', () => {
     // Arrange
-    render( <EntryForm /> );
+    render(
+        <TodosContext.Provider value={store}>
+            <EntryForm />
+        </TodosContext.Provider>
+    );
 
     const input = screen.getByPlaceholderText(/your next task/i) as HTMLInputElement;
     console.log(input.value);
-     
+
     // const button = screen.getByText(/add/i);
     const button = screen.getByRole('button');
 
@@ -19,13 +29,17 @@ test('Button disabled - when input text is empty', () => {
 
 test('Button enabled - when input text is not empty', () => {
     // Arrange
-    render( <EntryForm /> );
+    render(
+        <TodosContext.Provider value={store}>
+            <EntryForm />
+        </TodosContext.Provider>
+    );
 
-    const input = screen.getByPlaceholderText(/your next task/i) as HTMLInputElement;    
+    const input = screen.getByPlaceholderText(/your next task/i) as HTMLInputElement;
     const button = screen.getByRole('button');
 
     // Act 
-    fireEvent.change(input, {target: {value: 'buy milk'}})
+    fireEvent.change(input, { target: { value: 'buy milk' } })
 
     // Assert
     expect(button).not.toBeDisabled();
